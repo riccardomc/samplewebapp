6eabcb7495c25378882fed84d12425de31b1af5d
Riccardo M. Cefala <riccardo.cefala@gmail.com>
2017-10-24 10:35:40 +0200

    add hostname and request counter

--- main.go
+++ main.go
@@ -3,13 +3,22 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
+)
+
+var (
+	requestCounter int
+	hostname       string
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+	requestCounter++
+	fmt.Fprintf(w, "Hi there, got %d requests, running on %s, I love %s!", requestCounter, hostname, r.URL.Path[1:])
 }
 
 func main() {
+	requestCounter = 0
+	hostname = os.Getenv("HOSTNAME")
 	http.HandleFunc("/", handler)
 	http.ListenAndServe(":8080", nil)
 }
