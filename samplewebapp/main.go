225cab64c4e2da54aa6361bbceda46e3fd20be18
Riccardo M. Cefala <riccardo.cefala@gmail.com>
2017-11-02 09:49:40 +0100

    add /data/ endpoint to samplewebapp

--- samplewebapp/main.go
+++ samplewebapp/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"os"
@@ -16,9 +17,21 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hi there, got %d requests, running on %s, I love %s!", requestCounter, hostname, r.URL.Path[1:])
 }
 
+func data(w http.ResponseWriter, r *http.Request) {
+	j, err := json.Marshal([]string{"this", "and", "that"})
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Write(j)
+}
+
 func main() {
 	requestCounter = 0
 	hostname = os.Getenv("HOSTNAME")
 	http.HandleFunc("/", handler)
+	http.HandleFunc("/data/", data)
 	http.ListenAndServe(":8080", nil)
 }
